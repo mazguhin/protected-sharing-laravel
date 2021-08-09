79f3b409d0363db3e214588afea02c2360fad909
Evgeny Mazguhin <es.mazguhin@zbrsk.ru>
2021-08-09 14:21:12 +0500

    recipient
    tests

--- tests/Feature/Recipient/RecipientTest.php
+++ tests/Feature/Recipient/RecipientTest.php
@@ -12,6 +12,39 @@ class RecipientTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase, WithFaker;
 
+    public function test_get_all()
+    {
+        Recipient::factory()->count(3)->create();
+
+        $response = $this->get(route('recipient.get-all'));
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['success' => true]);
+
+        // TODO: проверить получателей в массиве recipients
+    }
+
+    public function test_get_active()
+    {
+        Recipient::factory()->count(3)->create();
+
+        $response = $this->get(route('recipient.get-active'));
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['success' => true]);
+
+        // TODO: проверить получателей в массиве recipients
+    }
+
+    public function test_get_active_empty_list()
+    {
+        Recipient::factory()->inactive()->count(3)->create();
+
+        $response = $this->get(route('recipient.get-active'));
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['success' => true]);
+
+        // TODO: проверить получателей в массиве recipients
+    }
+
     public function test_store()
     {
         $recipient = Recipient::factory()->make();
